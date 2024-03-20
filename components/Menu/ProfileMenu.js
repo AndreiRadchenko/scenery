246a0a50c0e129986fe969c99596a6e6a99723c2
andreiradchenko <d4100kx@gmail.com>
2024-03-20 22:21:32 +0200

    feat: Implement share image

--- components/Menu/ProfileMenu.js
+++ components/Menu/ProfileMenu.js
@@ -1,30 +1,21 @@
 import { BottomMenuBuilder } from '../BottomMenuBuilder/BottomMenuBuilder';
+import { onImageShare } from '../../services/shareService';
 
 export const ProfileMenu = ({ id, image, author }) => {
-  return (
-    new BottomMenuBuilder()
-      .addMenuItem({
-        ioniconsName: 'share-social',
-        text: 'Share via',
-        handler: () => {
-          console.log('Share image via: ', image.url);
-        },
-      })
-      // .addMenuItem({
-      //   ioniconsName: 'person',
-      //   text: 'Filter by user',
-      //   handler: () => {
-      //     console.log('Filter by user');
-      //     dismiss();
-      //   },
-      // })
-      .addMenuItem({
-        ioniconsName: 'trash',
-        text: 'Delete post',
-        handler: () => {
-          console.log('Delete post');
-        },
-      })
-      .build()
-  );
+  return new BottomMenuBuilder()
+    .addMenuItem({
+      ioniconsName: 'share-social',
+      text: 'Share via',
+      handler: () => {
+        onImageShare(image.url, id);
+      },
+    })
+    .addMenuItem({
+      ioniconsName: 'trash',
+      text: 'Delete post',
+      handler: () => {
+        console.log('Delete post');
+      },
+    })
+    .build();
 };
